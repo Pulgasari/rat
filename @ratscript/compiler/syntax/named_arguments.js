14eb660fd135e0a2524b9d14f5270feca178af1d
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-04 00:53:08 +0200

    Update named_arguments.js

--- @ratscript/compiler/syntax/named_arguments.js
+++ @ratscript/compiler/syntax/named_arguments.js
@@ -1,9 +1,18 @@
 // @ratscript/compiler/syntax/named_arguments.js
 
 export default function transform (code) {
+
+  // Define a Function
   const regexPattern = /(\w+)\s*\(([^)]*:[^)]*)\)/g;
-  
   code = code.replace(regexPattern, (match, funcName, body) => {  
+    return (body.trim().startsWith('{'))
+      ? match
+      : `${funcName}({ ${body} })`;
+  });
+
+  // Call a Function
+  const funcCallNamedRegex = /(\w+)\s*\(([^)]*:[^)]*)\)/g;
+  code = code.replace(funcCallNamedRegex, (match, funcName, body) => {
     return (body.trim().startsWith('{'))
       ? match
       : `${funcName}({ ${body} })`;
