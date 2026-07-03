9ed603fcf1828d0c9e5d6984134da2b3cac9be94
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-04 01:31:18 +0200

    Update index.js

--- @ratscript/compiler/index.js
+++ @ratscript/compiler/index.js
@@ -1,10 +1,11 @@
 // @ratscript/compiler
 
-import transform__cond             from './syntax/cond.js';
-import transform__guard            from './syntax/guard.js';
-import transform__match            from './syntax/match.js';
-import transform__named_arguments  from './syntax/named_arguments.js';
-import transform__signals          from './syntax/signals.js';
+import transform__cond              from './syntax/cond.js';
+import transform__guard             from './syntax/guard.js';
+import transform__match             from './syntax/match.js';
+import transform__multiline_strings from './syntax/multiline_strings.js';
+import transform__named_arguments   from './syntax/named_arguments.js';
+import transform__signals           from './syntax/signals.js';
 
 let _sig = str => '__' + str;
 
@@ -15,11 +16,12 @@ export function compile (code) {
                + `import { createCond, condMap } from './cond.js';\n\n`;
 
   //
-  code = transform__cond            (code);
-  code = transform__named_arguments (code);
-  code = transform__match           (code);
-  code = transform__guard           (code);
-  code = transform__signals         (code);
+  coye = transform__multiline_strings (code);
+  code = transform__cond              (code);
+  code = transform__named_arguments   (code);
+  code = transform__match             (code);
+  code = transform__guard             (code);
+  code = transform__signals           (code);
 
   //
   jsOutput += code;
