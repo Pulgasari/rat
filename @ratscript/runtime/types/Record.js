470ff6c05bd3a7bade11e807e72acbe76b20cc13
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-04 22:20:56 +0200

    Update Record.js

--- @ratscript/runtime/types/Record.js
+++ @ratscript/runtime/types/Record.js
@@ -1,22 +1,52 @@
 // @ratscript/runtime/types/Record.js
 
 export default class Record {
-  
+
+  // init
   constructor (struct, values) {
     this.struct = struct;
+
+    // validate using Struct
     struct.validate(values);
-    this.values = { ...values };
+
+    // store immutable values
+    this.values = Object.freeze({ ...values });
+
+    // expose keys as properties (optional but nice)
+    for (let key of struct.keys()) {
+      Object.defineProperty(this, key, {
+        value: this.values[key],
+        enumerable: true
+      });
+    }
+
+    Object.freeze(this);
+  }
+
+  // iterator
+  [Symbol.iterator]() {
+    return Object.values(this.values)[Symbol.iterator]();
   }
 
   get (key) {
     return this.values[key];
   }
 
-  set (key, value) {
-    this.struct.validate({ ...this.values, [key]: value });
-    this.values[key] = value;
-    return this;
+  
+
+  keys () {
+    return Object.keys(this.values);
   }
+
+  entries () {
+    return Object.entries(this.values);
+  }
+
+  toObject () {
+    return { ...this.values };
+  }
+
+  
 
   with (key, value) {
     const newValues = { ...this.values, [key]: value };
@@ -24,8 +54,34 @@ export default class Record {
     return new Record(this.struct, newValues);
   }
 
-  toObject () {
-    return { ...this.values };
+  merge (obj) {
+    const newValues = { ...this.values, ...obj };
+    this.struct.validate(newValues);
+    return new Record(this.struct, newValues);
+  }
+
+  toMerged (obj) {
+    return this.merge(obj);
+  }
+
+  // checks
+  equals (other) {
+    if (!(other instanceof Record))   return false;
+    if (other.struct !== this.struct) return false;
+
+    const keys = this.keys();
+    for (let key of keys) {
+      if (this.values[key] !== other.values[key]) return false;
+    }
+    return true;
+  }
+  has (key) {
+    return key in this.values;
   }
   
-}
+  // debug
+  toString () {
+    return `Record(${JSON.stringify(this.values)})`;
+  }
+  
+};
