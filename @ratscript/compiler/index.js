649970c11f36b539f3987358425ce58d311b582c
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-05 23:15:52 +0200

    Update index.js

--- @ratscript/compiler/index.js
+++ @ratscript/compiler/index.js
@@ -5,6 +5,7 @@ import transform__guard             from './syntax/guard.js';
 import transform__match             from './syntax/match.js';
 import transform__multiline_strings from './syntax/multiline_strings.js';
 import transform__named_arguments   from './syntax/named_arguments.js';
+import transform__pipe_operator     from './syntax/pipe_operator.js';
 import transform__signals           from './syntax/signals.js';
 
 let _sig = str => '__' + str;
@@ -17,6 +18,7 @@ export function compile (code) {
 
   //
   code = transform__multiline_strings (code);
+  code = transform__pipe_operator     (code);
   code = transform__cond              (code);
   code = transform__named_arguments   (code);
   code = transform__match             (code);
