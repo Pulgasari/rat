d18866ef68216fe64032dc92b8aea4ddaa6ab481
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-07 23:38:07 +0200

    Update and rename helpers.js to index.js

--- @ratscript/runtime/helpers.js
+++
@@ -1,5 +0,0 @@
-// @ratscript/runtime/helpers.js
-
-export { default as _assign } from './helpers/_assign.js';
-export { default as _inc    } from './helpers/_inc.js';
-export { default as _is     } from './helpers/_is.js';
---
+++ @ratscript/runtime/index.js
@@ -0,0 +1,13 @@
+// @ratscript/runtime/index.js
+
+export { default as _assign } from './helpers/_assign.js';
+export { default as _inc    } from './helpers/_inc.js';
+export { default as _is     } from './helpers/_is.js';
+
+//
+export { default as Enum   } from './types/Enum.js';
+export { default as List   } from './types/List.js';
+export { default as Record } from './types/Record.js';
+export { default as Struct } from './types/Struct.js';
+export { default as Tuple  } from './types/Tuple.js';
+export { default as Union  } from './types/Union.js';
