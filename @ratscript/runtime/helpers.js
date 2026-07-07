d98f4014c796a8baef4daba5a9729cce2453d197
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-07 18:59:10 +0200

    Update helpers.js

--- @ratscript/runtime/helpers.js
+++ @ratscript/runtime/helpers.js
@@ -1,5 +1,46 @@
 // @ratscript/runtime/helpers.js
 
+// +=
+export function _assign (left, right) {
+  //
+  if (left === undefined || left === null) return right;
+
+  // Number | String
+  if (typeof left === 'number' || typeof left === 'string') {
+    return left + right;
+  }
+
+  // Array
+  if (Array.isArray(left)) {
+    left.push(right);
+    return left;
+  }
+
+  // Map
+  if (left instanceof Set) {
+    left.add(right);
+    return left;
+  }
+
+  // Map
+  if (left instanceof Map) {
+    if (Array.isArray(right) && right.length === 2) {
+      left.set(right[0], right[1]); // map += ['key', 'value']
+    } else if (typeof right === 'object' && right !== null) {
+      for (let [k, v] of Object.entries(right)) left.set(k, v); // map += { key: 'value' }
+    }
+    return left;
+  }
+
+  // Object
+  if (typeof left === 'object') {
+    return Object.assign(left, right);
+  }
+
+  // Fallback
+  return left + right; 
+}
+
 export function _inc (needle, haystack) {
   if (!haystack) return false;
   if (Array.isArray(haystack) || typeof haystack === 'string') {
