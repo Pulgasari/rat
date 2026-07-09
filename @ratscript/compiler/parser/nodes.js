083eede1c55c9d6bfbd02de0037fd5b1e64dc563
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-09 19:16:52 +0200

    Update nodes.js

--- @ratscript/compiler/parser/nodes.js
+++ @ratscript/compiler/parser/nodes.js
@@ -1,6 +1,62 @@
 // @ratscript/compiler/parser/nodes.js
 
 export function createProgram(body) {
+  return { type: 'Program', body };
+}
+
+export function createBlock(statements) {
+  return { type: 'BlockStatement', body: statements };
+}
+
+export function createSiftStatement(init, cases, catchBlock, finallyBlock) {
+  return { type: 'SiftStatement', init, cases, catchBlock, finallyBlock };
+}
+
+export function createMoldStatement(target, init, cases, catchBlock, finallyBlock) {
+  return { type: 'MoldStatement', target, init, cases, catchBlock, finallyBlock };
+}
+
+export function createTraitDeclaration(name, body) {
+  return { type: 'TraitDeclaration', name, body };
+}
+
+export function createFunctionDeclaration(name, params, traits, body) {
+  return { type: 'FunctionDeclaration', name, params, traits, body };
+}
+
+export function createForStatement(initializer, isNaked, body) {
+  return { type: 'ForStatement', initializer, isNaked, body };
+}
+
+export function createRangeExpression(from, to) {
+  return { type: 'RangeExpression', from, to };
+}
+
+export function createTraitUseExpression(expression, traitName) {
+  return { type: 'TraitUseExpression', expression, traitName };
+}
+
+export function createExpressionStatement(expression) {
+  return { type: 'ExpressionStatement', expression };
+}
+
+export function createIdentifier(name) {
+  return { type: 'Identifier', name };
+}
+
+export function createLiteral(kind, value) {
+  return { type: 'Literal', kind, value };
+}
+
+export function createMemberExpression(object, property) {
+  return { type: 'MemberExpression', object, property };
+}
+
+export function createCallExpression(callee, args) {
+  return { type: 'CallExpression', callee, arguments: args };
+}
+
+export function createProgram(body) {
   return {
     type: 'Program',
     body
