1d4cea01cc18572bdd86295508fc6f5114811064
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-09 05:42:05 +0200

    Create meta.js

---
+++ @ratscript/compiler/meta.js
@@ -0,0 +1,35 @@
+// @ratscript/compiler/meta.js
+
+export const keywords = [
+  'alias',
+  'async',
+  'await',
+  'case',
+  'catch',
+  'cond',
+  'const',
+  'do',
+  'enum',
+  'export',
+  'finally',
+  'fn',
+  'for',
+  'function',
+  'if',
+  'import',
+  'let',
+  'proxy',
+  'return',
+  'struct',
+  'switch',
+  'try',
+  'var',
+  'while',
+];
+
+export xxx = [
+  'Array',
+  'Object',
+  'Number',
+  'String',
+];
