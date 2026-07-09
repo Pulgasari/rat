230d6cfcd4dab48b6a6b0e3f29700156e4daa76a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-09 05:51:44 +0200

    Update index.js

--- @ratscript/runtime/index.js
+++ @ratscript/runtime/index.js
@@ -1,6 +1,7 @@
 // @ratscript/runtime/index.js
 
 export { default as _assign } from './helpers/_assign.js';
+export { default as _fn     } from './helpers/_fn.js';
 export { default as _inc    } from './helpers/_inc.js';
 export { default as _is     } from './helpers/_is.js';
 
