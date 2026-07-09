1fcf9adbfebb6ec8057a12a8e60109172a196d21
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-09 20:58:24 +0200

    Create utils.js

---
+++ @ratscript/compiler/utils.js
@@ -0,0 +1,31 @@
+// @ratscript/compiler/utils.js
+
+import * as NodeTypes from './nodes.js';
+
+  
+export function createNode (type, args = {}) {
+  const def = NodeTypes[type];
+  if (!def) throw new Error(`Unknown node type: ${type}`);
+
+  const node = { type: def.type };
+
+  for (const [key, meta] of Object.entries(def.args)) {
+    if (args[key] !== undefined) {
+      node[key] = args[key];
+    } else if (meta.default !== undefined) {
+      node[key] = meta.default;
+    } else if (meta.required) {
+      throw new Error(`Missing required argument '${key}' for node '${type}'`);
+    } else {
+      node[key] = null;
+    }
+  }
+
+  return node;
+}
+
+export const create = new Proxy({}, {
+  get(_, prop) {
+    return (args = {}) => createNode(prop, args);
+  }
+});
