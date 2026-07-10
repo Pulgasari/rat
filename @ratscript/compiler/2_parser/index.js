f625a264845e0126b15a563a9d07ad8a72613f19
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-10 23:49:34 +0200

    Update index.js

--- @ratscript/compiler/2_parser/index.js
+++ @ratscript/compiler/2_parser/index.js
@@ -1,5 +1,50 @@
 // @ratscript/compiler/2_parser/index.js
 
+import { init, isEOF }   from './state.js';
+import { ASTNode }       from './../utils.js';
+import * as declarations from './declarations.js';
+import * as expressions  from './expressions.js';
+import * as statements   from './statements.js';
+
+function parseBody () {
+  const body = [];
+  while (!isToken('}') && !isEOF()) {
+    body.push(parseStatement());
+  }
+  return body;
+}
+
+export function parse (tokens) {
+  init(tokens);
+  return ASTNode.Program({ body: parse.Body });
+}
+
+export const parsed = {
+  ...declarations,
+  ...expressions,
+  ...statements,
+  parseBody,
+};
+
+for (const [key, body] of Object.entries(parsed)) {
+  const name = key.replace(/^parse/, '');
+  
+  Object.defineProperty(parsed, name, { 
+    get () { return body(); },
+    enumerable: true
+  });
+  
+  delete parsed[key];
+}
+
+parsed.call = name => parsed[name];
+
+
+
+
+
+
+/*
 import { init, isEOF }    from './state.js';
 import { parseStatement } from './statements.js';
 import { ASTNode }        from './../utils.js';
@@ -16,7 +61,7 @@ export function parseBody () {
   }
   return body;
 }
-
+*/
 /*
 export function parse (tokens) {
   init(tokens);
@@ -29,77 +74,3 @@ export function parse (tokens) {
   return ASTNode.Program({ body });
 }
 */
-
-
-
-
-
-
-import * as declarations from './declarations.js';
-import * as expressions  from './expressions.js';
-import * as statements   from './statements.js';
-
-const methods = { 
-  ...declarations,
-  ...expressions,
-  ...statements,
-};
-
-const Expression = () => parse.Assignment;
-
-const Body = () => {
-  const body = [];
-  while (!isToken('}') && !isEOF()) {
-    body.push(parse('Statement'));
-    body.push(parse.Statement());
-    body.push(parsed.Statement);
-    parse.Statement |> body.push;
-  }
-  return body;
-}
-
-export const parse = name => methods[name]();
-export const parse = name => methods['parse' + name]();
-
-parse('Body');
-parse('Expression');
-parse('For');
-
-parse.Body();
-parse.Expression();
-parse.For();
-
-parse.Body;
-parse.Expression;
-parse.For;
-
-parsed.Body;
-parsed.Expression;
-parsed.For;
-
-
-
-
-
-import * as declarations from './declarations.js';
-import * as expressions  from './expressions.js';
-import * as statements   from './statements.js';
-
-export const parsed = {
-  ...declarations,
-  ...expressions,
-  ...statements,
-};
-
-for (const [key, body] of Object.entries(parsed)) {
-  const name = key.replace(/^parse/, '');
-  
-  Object.defineProperty(parsed, name, { 
-    get () { return body(); },
-    enumerable: true
-  });
-  
-  delete parsed[key];
-}
-
-parsed.call = name => parsed[name];
