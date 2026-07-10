8ca723437192628ddc60625b580aebda3d69a8a9
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-10 13:28:06 +0200

    Create helpers.js

---
+++ @ratscript/compiler/generator/helpers.js
@@ -0,0 +1,9 @@
+// @ratscript/compiler/generator/helpers.js
+
+export function indent (code, level = 1) {
+  const pad = '  '.repeat(level);
+  return code
+    .split('\n')
+    .map(line => line.length ? pad + line : line)
+    .join('\n');
+}
