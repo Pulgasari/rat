1f28d1fa1a1e1ac6af4d1b9dd4c834a0873bc1b9
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-11 14:58:37 +0200

    Update index.js

--- @ratscript/compiler/generator/index.js
+++ @ratscript/compiler/generator/index.js
@@ -1,21 +1,21 @@
 // @ratscript/compiler/generator/index.js
 
-import { evilFactory }   from './../utils.js'|
-import * as declarations from './declarations.js';
-import * as expressions  from './expressions.js';
-import * as statements   from './statements.js';
+import { createEvilFactory } from './../utils.js'|
+import * as declarations     from './declarations.js';
+import * as expressions      from './expressions.js';
+import * as statements       from './statements.js';
 
-export const generated = evilFactory({ 
+export const generated = createEvilFactory ({ 
   prefix: 'generate',
-  source: [declarations, expressions, statements]
+  source: [declarations, expressions, statements],
 });
 
 
 export function generate (node) {
   if (!node) return '';
 
-  const fn = generators[node.type];
-  if (!fn) throw new Error(`[Generator-Fehler]: Unbekannter AST-Knoten-Typ "${node.type}"`);
+  const fn = generated[node.type];
+  if (!fn) throw new Error(`[Generator Error]: Unnknown ASTNode-Type "${node.type}"`);
 
   return fn(node);
 }
