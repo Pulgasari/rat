83ffc16e6cbe984cfed463f3a19e2c7a3bb0a9fa
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-11 15:32:42 +0200

    Update index.js

--- @ratscript/compiler/2_parser/index.js
+++ @ratscript/compiler/2_parser/index.js
@@ -1,76 +1,20 @@
 // @ratscript/compiler/2_parser/index.js
 
-import { init, isEOF }   from './state.js';
-import { ASTNode }       from './../utils.js';
+import { ASTNode }           from './../utils.js';
+import { createEvilFactory } from './../utils.js';
+import { init }              from './state.js';
+
+import * as core         from './core.js';
 import * as declarations from './declarations.js';
 import * as expressions  from './expressions.js';
 import * as statements   from './statements.js';
 
-
+export const parsed = createEvilFactory({
+  prefix: 'parse',
+  source: [core, declarations, expressions, statements]
+});
 
 export function parse (tokens) {
   init(tokens);
   return ASTNode.Program({ body: parsed.Body });
 }
-
-export const parsed = {
-  ...declarations,
-  ...expressions,
-  ...statements,
-  parseBody,
-};
-
-for (const [key, body] of Object.entries(parsed)) {
-  const name = key.replace(/^parse/, '');
-  
-  Object.defineProperty(parsed, name, { 
-    get () { return body(); },
-    enumerable: true
-  });
-  
-  delete parsed[key];
-}
-
-parsed.call = name => parsed[name];
-
-function parseBody () {
-  const body = [];
-  while (!isToken('}') && !isEOF()) {
-    body.push(parsed.Statement);
-  }
-  return body;
-}
-
-
-
-
-/*
-import { init, isEOF }    from './state.js';
-import { parseStatement } from './statements.js';
-import { ASTNode }        from './../utils.js';
-
-export function parse (tokens) {
-  init(tokens);
-  return ASTNode.Program({ body: parseBody() });
-}
-
-export function parseBody () {
-  const body = [];
-  while (!isToken('}') && !isEOF()) {
-    body.push(parseStatement());
-  }
-  return body;
-}
-*/
-/*
-export function parse (tokens) {
-  init(tokens);
-
-  const body = [];
-  while (!isEOF()) {
-    body.push(parseStatement());
-  }
-
-  return ASTNode.Program({ body });
-}
-*/
