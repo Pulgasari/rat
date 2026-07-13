9119e8925fe2de74302d8cf8ae3e21404fb440e6
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-14 01:30:29 +0200

    Update methods.js

--- @ratscript/compiler/parser/methods.js
+++ @ratscript/compiler/parser/methods.js
@@ -46,10 +46,33 @@ export function parseStatement (ctx) {
 
 // :::::: PATTERNS
 
+export function parseBlock (ctx) {
+  ctx.consume('{');
+  const body = ctx.parseBody();
+  ctx.consume('}');
+  return ASTNode.BlockStatement({ body });
+  
+  // const body = ctx.parseWrapped('braces', () => ctx.parseBody());
+  // return ASTNode.BlockStatement({ body });
+
+  // return ASTNode.BlockStatement({ 
+  //   body: ctx.parseWrapped('braces', () => ctx.parseBody())
+  // });
+}
+
+export function parseActionBlock (ctx) {
+  if (ctx.match('{')) {
+    const body = ctx.parseBody();
+    ctx.consume('}');
+    return ASTNode.BlockStatement({ body });
+  }
+  return ASTNode.BlockStatement({ body: [ctx.parseStatement()] });
+}
+
 export function parseBody (ctx) {
   const body = [];
   while (!ctx.checkAny('}', 'EOF')) {
-    body.push(parsed.Statement);
+    body.push(ctx.parseStatement());
   }
   return body;
 }
@@ -60,11 +83,23 @@ export function parseBody (ctx) {
 
 function parseLabeledStatement (ctx) {
   const labelToken = advance(); // Label-Identifier
-  advance(); // ':'
+  ctx.advance(); // ':'
   const body = ctx.parseStatement();
   return ASTNode.LabeledStatement({ label: labelToken.value, body });
 }
 
 // :::::: EXPRESSIONS
+
+// :::::: KINDA WEIRD
+
+export function parseConditionTest (ctx) {
+  const expr = ctx.parseExpression();
+  if (ctx.check('as')) {
+    ctx.advance(); // 'as'
+    const name = ctx.consume('IDENTIFIER').value;
+    return ASTNode.AsBindingExpression({ expr, name });
+  }
+  return expr;
+}
 
 // :::::: INTERNAL HELPERS
